e4468fef8e74ae4963b50520df06f2c604420b1d
Pav0l <pavol.karas@gmail.com>
2019-03-26 13:20:03 +0100

    POST request that adds new Zoo to the DB

--- index.js
+++ index.js
@@ -15,6 +15,20 @@ server.use(helmet());
 
 // endpoints here
 
+server.post('/api/zoos', async (req, res) => {
+  const zoo = req.body;
+  if (zoo.name) {
+    try {
+      const newZoo = await knex('zoos').insert(req.body);
+      res.status(201).json({message: `New Zoo created with an ID ${newZoo}`});
+    } catch (error) {
+      res.status(500).json({ error });
+    }
+  } else {
+    res.status(400).json({ message: 'Please include name inside the body of the request' });
+  }
+});
+
 
 const port = 3300;
 server.listen(port, function() {
